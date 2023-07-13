35ebd2a5e870c2ca09eb18b772ce32f29aa537d6
Serenitybleps <fawkesphoenix21@gmail.com>
2023-07-13 10:05:02 -0700

    Added background and padding, "finished" app

--- STEMspiration/ContentView.swift
+++ STEMspiration/ContentView.swift
@@ -9,13 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+       
+        ZStack {
+            
+            Color(.systemBlue)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 20.0) {
+                        Image("SallyRide")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius (15)
+                            
+                    HStack {
+                        Text("Sally Ride")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                       
+                        Text("Astronaut")
+                            .fontWeight(.semibold)
+                            
+                    
+                        }
+                        
+                        
+                        Text("Was the first American woman in space and the youngest at the time, being only 32. ")
+                        
+                    }
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white))
+            .cornerRadius(15)
+            .shadow(radius: 15)
+            .padding()
+            
+            
+        
         }
-        .padding()
+        
+        
+        
     }
 }
 
